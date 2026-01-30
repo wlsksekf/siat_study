8702d1b6ccc03e630725707fbbdbd762cf1457c9
wlsksekf <jinaniyagi@gmail.com>
2026-01-30 11:17:02 +0900

    1

---
+++ 01/30/python_3.12.9_start/comment.py
@@ -0,0 +1,2 @@
+# 이 코드는 Hello World!를 실행함
+print("Hello World!")
---
+++ 01/30/python_3.12.9_start/module.py
@@ -0,0 +1,21 @@
+"""
+모듈이란 함수/클래스/변수를 담아 재사용할 수 있도록 만든 파이썬 파일(.py)입니다.
+
+이 모듈은 사칙연산 기능을 제공합니다.
+"""
+
+def add(a, b):
+    """두 수를 더합니다."""
+    return a + b
+
+def subtract(a, b):
+    """두 수를 뺍니다."""
+    return a - b
+
+def multiply(a, b):
+    """두 수를 곱합니다."""
+    return a * b
+
+def divide(a, b):
+    """두 수를 나눕니다."""
+    return a / b
